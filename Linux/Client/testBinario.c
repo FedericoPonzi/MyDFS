25b660c7cdc7043b3f96794e3ac32a9b3d2e21f4
FedericoPonzi <isaacisback92@gmail.com>
2015-10-05 07:23:15 +0200

    Fixa il test.

--- Linux/Client/testBinario.c
+++ Linux/Client/testBinario.c
@@ -24,7 +24,7 @@ int main()
 
     while((i = mydfs_read(id, MYSEEK_CUR, buffer, 60000)) > 0)
     {
-        fwrite(buffer, 1, 60000, fp);
+        fwrite(buffer, 1, i, fp);
     }
     mydfs_close(id);
     fclose(fp);
