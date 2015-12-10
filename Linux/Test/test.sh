eb44ba221391953a2111f17d884f573f632a21a9
FedericoPonzi <isaacisback92@gmail.com>
2015-12-10 11:57:21 +0100

    Rimuove file inutilizzati

--- Linux/Test/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-x=1
-while [ $x -le 1000 ]
-do
-  echo "Ciaoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" >> /tmp/test.txt
-  x=$(( $x + 1 ))
-done
